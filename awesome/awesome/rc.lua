3782c6d4bc775b8504cd00d2089aa6af5d3ab670
Martin Baeuml <baeuml@kit.edu>
2011-06-06 16:39:55 +0200

    awesome: disable most layouts

--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -26,18 +26,18 @@ beautiful.init(home .. "/.dotfiles/awesome/awesome/themes/zoop/theme.lua")
 -- Layouts
 layouts =
 {
-    awful.layout.suit.floating,
+    -- awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
+    -- awful.layout.suit.tile.bottom,
+    -- awful.layout.suit.tile.top,
+    -- awful.layout.suit.fair,
+    -- awful.layout.suit.fair.horizontal,
+    -- awful.layout.suit.spiral,
+    -- awful.layout.suit.spiral.dwindle,
+    -- awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier
+    -- awful.layout.suit.magnifier
 }
 -- }}}
 
