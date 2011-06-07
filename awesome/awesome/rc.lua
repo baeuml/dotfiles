1215785e20a33df3116d7fb63b2ed02deebed4b2
Martin Baeuml <baeuml@kit.edu>
2011-06-07 13:18:54 +0200

    awesome: rule to make the control window of okapi-viewer floating

--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -306,6 +306,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
+    { rule = { name = "okapi-viewer" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
